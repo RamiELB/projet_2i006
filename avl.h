98bd2508385b05a3512a5217bd638da3d8fa67a7
EdwinSri <edwinsri@gmail.com>
2019-03-22 16:29:57 +0100

    avl

---
+++ avl.h
@@ -0,0 +1,19 @@
+#ifndef AVL_H
+#define AVL_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <netlist.h>
+
+
+typedef struct noeud {
+    Segment * seg;
+    int y; /* Ordonnee du segment */
+    struct noeud * fg; /* Fils gauche */
+    struct noeud * fd; /* Fils droit */
+    int hauteur;
+} Noeud;
+
+typedef Noeud* ABR;
+
+
+#endif /* NETLIST_H */
